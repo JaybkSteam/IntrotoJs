819945d4528be8da60c01965df1a5d28a5ccae72
Jaybksteam <jedouard@brooklynsteamcenter.org>
2023-04-17 14:54:45 -0400

    Changes

---
+++ grade.js
@@ -0,0 +1,23 @@
+let myGrade =  90
+
+if (myGrade == 90){
+    console.log ("You have a A")
+}
+else if (myGrade == 80){
+    console.log ("You have a b")
+}
+else if (myGrade === 70 ) {
+console.log ("You have a C") }
+
+else if (myGrade == 60){
+console.log ("You have a D")}
+
+else if (myGrade == 50){
+    console.log("You have a F")
+}
+
+
+
+
+
+ 
