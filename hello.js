d9819858d38f7232f66d15b06dc1cc89ad3989b7
Jaybksteam <jedouard@brooklynsteamcenter.org>
2023-04-17 14:54:57 -0400

    changes

---
+++ hello.js
@@ -0,0 +1,20 @@
+console.log("Hello World")
+
+let myName = "Jaylin Edouard"
+
+let myAge = 16
+
+console.log("Hello My name is " + myName + ". My age is " + myAge + ".")
+
+if (myAge ==0) {
+   console.log ("I was born yesterday")
+
+} else if (myAge < 18){
+    console.log("I was not born yesterday")
+}
+
+let myList =("Pokemon, Yugioh, Digimon")
+
+for (let item of myList){
+    console.log("These series which are " + myList + " are some of my favorite to watch while growing up.")
+}
