8d2336d24311e3a457808872efcc87d680cb3dd7
Jaybksteam <jedouard@brooklynsteamcenter.org>
2023-04-28 14:56:08 -0400

    Change

---
+++ stopwatch.js
@@ -0,0 +1,11 @@
+function start () {
+    alert("You clicked the start button") 
+
+}
+function stop () {
+    alert("You clicked the Stop Button")
+}
+function reset ({
+    alert("You clicked the reset button")
+
+})
